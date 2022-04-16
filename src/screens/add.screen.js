9d9c9d2de8288d9326af9a7263386d58f0d76165
Shivesh Tiwari <yuvanastra@Shiveshs-MacBook-Air.local>
2022-04-16 07:53:33 +0530

    added AddDocument and Profile screen

---
+++ src/screens/add.screen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  View,
+  Text,
+} from 'react-native'
+
+const AddDocument = () => {
+
+  return(
+    <Text> this is AddDocument </Text>
+  )
+}
+
+export default AddDocument
+
