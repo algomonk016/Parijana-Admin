37c2ce701a7681376cfe54cc623db98d889e3ccb
Shivesh Tiwari <yuvanastra@Shiveshs-MacBook-Air.local>
2022-04-17 09:47:42 +0530

    added gifs

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,23 @@
 const profile = require("../assets/images/shivesh.jpeg")
 const digitalDocument1 = require('../assets/images/digitalDocument.jpeg')
 const digitalDocument2 = require('../assets/images/digitalDocument2.jpeg')
+const beanWaiting = require('../assets/images/waiting.jpg')
+const waiting = require('../assets/images/waiting2.png')
+const bookGIF = require('../assets/images/Book.gif')
+const pdfGIF = require('../assets/images/pdf.gif')
+const paperGIF = require('../assets/images/papers.gif')
+const sharinganWaiting = require('../assets/images/sharinganWaiting.gif')
+const kakashiThumbsUp = require('../assets/images/kakashi-thumbs-up.gif')
 
 export default {
     profile,
     digitalDocument1,
     digitalDocument2,
+    waiting,
+    beanWaiting,
+    bookGIF,
+    pdfGIF,
+    paperGIF,
+    sharinganWaiting,
+    kakashiThumbsUp
 }
